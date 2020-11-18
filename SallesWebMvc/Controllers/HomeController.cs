53002d3dfba4ffeb5d5d82aa5852004edc180dff
eduardo-cevoli <eduardocevoli@gmail.com>
2020-11-18 09:27:34 -0300

    MySQL adaptation and first migration

--- SallesWebMvc/Controllers/HomeController.cs
+++ SallesWebMvc/Controllers/HomeController.cs
@@ -4,10 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SallesWebMvc.Models;
-using SallesWebMvc.Models.ViewModels;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
-namespace SallesWebMvc.Controllers
+namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
     {
@@ -39,7 +39,7 @@ namespace SallesWebMvc.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new SallesWebMvc.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new SalesWebMvc.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
